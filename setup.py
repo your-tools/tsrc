eaeb414d63242e886437b19cb09423f7b2e39338
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-07-05 18:44:11 +0200

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+from setuptools import setup, find_packages
+
+if sys.version_info.major < 2:
+    sys.exit("Error: Please upgrade to Python3")
+
+setup(name="tsrc",
+      version="0.1",
+      description="Manage multiple repositories",
+      url="https://github.com/TankerApp/tsrc",
+      author="Kontrol SAS",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+        "colored_traceback",
+        "colorama",
+        "path.py",
+        "pyparsing",
+        "requests",
+        "ruamel.yaml",
+        "unidecode",
+      ],
+      entry_points={
+        "console_scripts": [
+          "tsrc = tsrc.cli.main:main",
+         ]
+      }
+      )
