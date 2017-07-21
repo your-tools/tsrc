6edddaaee5d9810a9216a91495ed4e8ad4754b17
Dimitri Merejkowsky <d.merej@gmail.com>
2017-07-21 18:57:30 +0200

    gitlab: better error message when project is not found

--- tsrc/gitlab.py
+++ tsrc/gitlab.py
@@ -64,7 +64,10 @@ class GitLabHelper():
 
     def get_project_id(self, project_name):
         encoded_project_name = urllib.parse.quote(project_name, safe=list())
-        res = self.make_request("GET", "/projects/%s" % encoded_project_name)
+        try:
+            res = self.make_request("GET", "/projects/%s" % encoded_project_name)
+        except GitLabError:
+            raise GitLabError("Could not find", project_name) from None
         return res["id"]
 
     def find_opened_merge_request(self, project_id, source_branch):
