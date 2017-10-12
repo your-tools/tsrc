0731209a2f03e23fd4a40414a7313cc90266ac8d
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-10-12 16:10:34 +0200

    Better CI: run all checks and don't stop at first failure

--- ci/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -xe
-
-export PYTHONPATH=.
-pycodestyle .
-python3 ci/run-pyflakes.py
-python3 ci/run-mccabe.py 10
-pylint tsrc --score=no
--- ci/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -xe
-export PYTHONPATH=.
-
-pytest
