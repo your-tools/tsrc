8b7ab9534ced4ed9585db4da21a44e38682276cb
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2017-11-15 15:09:43 +0100

    Start implementing github support

--- tsrc/config.py
+++ tsrc/config.py
@@ -14,7 +14,7 @@ def parse_config_file(file_path, config_schema):
     except OSError as os_error:
         raise tsrc.InvalidConfig(file_path, "Could not read file: %s" % str(os_error))
     try:
-        parsed = ruamel.yaml.safe_load(contents)
+        parsed = ruamel.yaml.load(contents, ruamel.yaml.RoundTripLoader)
     except ruamel.yaml.error.YAMLError as yaml_error:
         # pylint: disable=no-member
         context = "(ligne %s, col %s) " % (
@@ -23,20 +23,30 @@ def parse_config_file(file_path, config_schema):
         )
         message = "%s - YAML error: %s" % (context, yaml_error.context)
         raise tsrc.InvalidConfig(file_path, message)
-    try:
-        return config_schema.validate(parsed)
-    except schema.SchemaError as schema_error:
-        raise tsrc.InvalidConfig(file_path, str(schema_error))
+    if config_schema:
+        try:
+            return config_schema.validate(parsed)
+        except schema.SchemaError as schema_error:
+            raise tsrc.InvalidConfig(file_path, str(schema_error))
+
+
+def get_tsrc_config_path():
+    config_path = path.Path(xdg.XDG_CONFIG_HOME)
+    config_path = config_path.joinpath("tsrc.yml")
+    return config_path
+
+
+def dump_tsrc_config(config):
+    dumped = ruamel.yaml.dump(config, Dumper=ruamel.yaml.RoundTripDumper)
+    file_path.write_text(dumped)
 
 
 def parse_tsrc_config(config_path=None):
     auth_schema = {
-        "gitlab": {
-            "token": str
-        }
+        schema.Optional("gitlab"): {"token": str},
+        schema.Optional("github"): {"token": str},
     }
     tsrc_schema = schema.Schema({"auth": auth_schema})
     if not config_path:
-        config_path = path.Path(xdg.XDG_CONFIG_HOME)
-        config_path = config_path.joinpath("tsrc.yml")
+        config_path = get_tsrc_config_path()
     return parse_config_file(config_path, tsrc_schema)
---
+++ tsrc/github.py
@@ -0,0 +1,72 @@
+""" Helpers for github web API """
+
+
+import getpass
+import uuid
+
+import github3
+import ui
+
+import tsrc.config
+
+
+def get_previous_token():
+    config = tsrc.config.parse_tsrc_config()
+    auth = config.get("auth")
+    if not auth:
+        return None
+    github_auth = auth.get("github")
+    if not github_auth:
+        return None
+    return github_auth.get("token")
+
+
+def generate_token():
+    ui.info_1("Creating new GitHub token")
+    username = ui.ask_string("Please enter you GitHub username")
+    password = getpass.getpass("Password: ")
+
+    scopes = ['repo']
+
+    # Need a different note for each device, otherwise
+    # gh_api.authorize() will fail
+    note = "tsrc-" + str(uuid.uuid4())
+    note_url = "https://tankerapp.github.io/tsrc"
+
+    gh_api = github3.GitHub()
+    gh_api.login(username, password, two_factor_callback=lambda: ui.ask_string("2FA code: "))
+
+    user = gh_api.user()
+    auth = gh_api.authorize(user, password, scopes, note, note_url)
+    return auth.token
+
+
+def save_token(token):
+    cfg_path = tsrc.config.get_tsrc_config_path()
+    if cfg_path.exists():
+        config = tsrc.config.parse_tsrc_config()
+    else:
+        config = dict()
+    if "auth" not in config:
+        config["auth"] = dict()
+    auth = config["auth"]
+    if "github" not in config:
+        auth["github"] = dict()
+    auth["github"]["token"] = token
+    tsrc.config.dump_tsrc_config(config)
+
+
+def ensure_token():
+    token = get_previous_token()
+    if not token:
+        token = generate_token()
+        save_token(token)
+    return token
+
+
+def login():
+    token = ensure_token()
+    gh_api = github3.GitHub()
+    gh_api.login(token=token)
+    ui.info_2("Successfully logged in on GitHub with login", gh_api.user().login)
+    return gh_api
