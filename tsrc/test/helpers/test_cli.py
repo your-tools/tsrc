b7d2c5a7018710394b05fa8e81bce086bbecc146
Dimitri Merejkowsky <d.merej@gmail.com>
2018-10-13 19:05:27 +0200

    Remove redundancy in test function names

--- tsrc/test/helpers/test_cli.py
+++ tsrc/test/helpers/test_cli.py
@@ -1,9 +1,9 @@
 from tsrc.test.helpers.cli import CLI
 
 
-def test_tsrc_cli_help(tsrc_cli: CLI) -> None:
+def test_help(tsrc_cli: CLI) -> None:
     tsrc_cli.run("--help")
 
 
-def test_tsrc_cli_bad_args(tsrc_cli: CLI) -> None:
+def test_bad_args(tsrc_cli: CLI) -> None:
     tsrc_cli.run("bad", expect_fail=True)
--- tsrc/test/helpers/test_git_server.py
+++ tsrc/test/helpers/test_git_server.py
@@ -13,13 +13,13 @@ def read_remote_manifest(workspace_path: Path, git_server: GitServer) -> tsrc.Ma
     return manifest
 
 
-def test_git_server_add_repo_can_clone(workspace_path: Path, git_server: GitServer) -> None:
+def test_add_repo_can_clone(workspace_path: Path, git_server: GitServer) -> None:
     foobar_url = git_server.add_repo("foo/bar")
     tsrc.git.run(workspace_path, "clone", foobar_url)
     assert (workspace_path / "bar").exists()
 
 
-def test_git_server_can_add_copies(workspace_path: Path, git_server: GitServer) -> None:
+def test_can_add_copies(workspace_path: Path, git_server: GitServer) -> None:
     git_server.add_repo("foo")
     git_server.manifest.set_repo_file_copies("foo", [("foo.txt", "top.txt")])
     manifest = read_remote_manifest(workspace_path, git_server)
@@ -34,7 +34,7 @@ def test_can_configure_gitlab(tmp_path: Path, git_server: GitServer) -> None:
     assert manifest.gitlab["url"] == test_url
 
 
-def test_git_server_add_repo_updates_manifest(workspace_path: Path, git_server: GitServer) -> None:
+def test_add_repo_updates_manifest(workspace_path: Path, git_server: GitServer) -> None:
     git_server.add_repo("foo/bar")
     git_server.add_repo("spam/eggs")
     manifest = read_remote_manifest(workspace_path, git_server)
@@ -46,7 +46,7 @@ def test_git_server_add_repo_updates_manifest(workspace_path: Path, git_server: 
         assert "refs/heads/master" in out
 
 
-def test_git_server_change_manifest_branch(workspace_path: Path, git_server: GitServer) -> None:
+def test_change_manifest_branch(workspace_path: Path, git_server: GitServer) -> None:
     git_server.add_repo("foo")
     git_server.manifest.change_branch("devel")
     git_server.add_repo("bar")
@@ -61,7 +61,7 @@ def test_git_server_change_manifest_branch(workspace_path: Path, git_server: Git
     assert len(manifest.get_repos()) == 2
 
 
-def test_git_server_change_repo_branch(workspace_path: Path, git_server: GitServer) -> None:
+def test_change_repo_branch(workspace_path: Path, git_server: GitServer) -> None:
     foo_url = git_server.add_repo("foo")
     git_server.change_repo_branch("foo", "devel")
     git_server.push_file("foo", "devel.txt", contents="this is devel\n")
@@ -70,14 +70,14 @@ def test_git_server_change_repo_branch(workspace_path: Path, git_server: GitServ
     assert (foo_path / "devel.txt").text() == "this is devel\n"
 
 
-def test_git_server_tag(workspace_path: Path, git_server: GitServer) -> None:
+def test_tag(workspace_path: Path, git_server: GitServer) -> None:
     foo_url = git_server.add_repo("foo")
     git_server.tag("foo", "v0.1")
     _, out = tsrc.git.run_captured(workspace_path, "ls-remote", foo_url)
     assert "refs/tags/v0.1" in out
 
 
-def test_git_server_default_branch_devel(workspace_path: Path, git_server: GitServer) -> None:
+def test_default_branch_devel(workspace_path: Path, git_server: GitServer) -> None:
     foo_url = git_server.add_repo("foo", default_branch="devel")
     tsrc.git.run(workspace_path, "clone", foo_url)
     foo_path = workspace_path / "foo"
