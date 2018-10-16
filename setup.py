4def4bd409aac0396833693a835c7520f7526a3f
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-10-16 15:07:41 +0200

    Bump to 0.6.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_long_description():
 
 setup(
     name="tsrc",
-    version="0.6.1",
+    version="0.6.2",
     description="Manage multiple repositories",
     long_description=get_long_description(),
     url="https://github.com/SuperTanker/tsrc",
