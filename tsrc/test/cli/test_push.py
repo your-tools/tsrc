d1de47f5a39281b5551ad60affb7f08516542372
Dimitri Merejkowsky <d.merej@gmail.com>
2019-12-10 14:15:05 +0100

    test/cli/test_push: kill one mock
    We can use a in-memory Manifest config now :)

--- tsrc/test/cli/test_push.py
+++ tsrc/test/cli/test_push.py
@@ -1,5 +1,4 @@
 import argparse
-import mock
 
 from path import Path
 import tsrc
@@ -38,25 +37,30 @@ def test_push_use_given_push_spec(
     assert "heads/remote" in out
 
 
-def test_service_from_url() -> None:
-    workspace_mock = mock_workspace_git_urls()
+def get_service(url: str, manifest: tsrc.Manifest) -> Optional[str]:
+    return service_from_url(url, manifest=manifest)
 
-    def get_service(url: str) -> str:
-        return tsrc.cli.push.service_from_url(url, workspace_mock)
 
-    assert get_service("git@github.com:TankerHQ/tsrc.git") == "github"
-    assert get_service("git@gitlab.ex.co:TankerHQ/tsrc.git") == "gitlab"
-    assert get_service("git@github.ex.co:TankerHQ/tsrc.git") == "github_enterprise"
-    assert get_service("git@git.ex.co:TankerHQ/tsrc.git") == "git"
+def test_service_from_url_with_empty_manifest() -> None:
+    manifest = tsrc.Manifest()
 
-    workspace_mock.get_github_enterprise_url.return_value = (
-        "https://github.ex.co:8443/github"
-    )
-    workspace_mock.get_gitlab_url.return_value = "https://gitlab.ex.co:8443/gitlab"
-    assert get_service("git@gitlab.ex.co:TankerHQ/tsrc.git") == "gitlab"
-    assert get_service("git@github.ex.co:TankerHQ/tsrc.git") == "github_enterprise"
-    assert get_service("git@gitlab.ex.co:8443:TankerHQ/tsrc.git") == "gitlab"
-    assert get_service("git@github.ex.co:8443:TankerHQ/tsrc.git") == "github_enterprise"
+    assert get_service("git@github.com:TankerHQ/tsrc.git", manifest) == "github"
+    assert get_service("git@gitlab.ex.co:TankerHQ/tsrc.git", manifest) is None
+    assert get_service("git@github.ex.co:TankerHQ/tsrc.git", manifest) is None
+    assert get_service("git@git.ex.co:TankerHQ/tsrc.git", manifest) is None
+
+
+def test_service_from_url_with_manifest_config() -> None:
+    manifest = tsrc.Manifest()
+    manifest.github_enterprise_url = "https://github.ex.co:8443/github"
+    manifest.gitlab_url = "https://gitlab.ex.co:8443/gitlab"
+
+    # fmt: off
+    assert get_service("git@gitlab.ex.co:TankerHQ/tsrc.git", manifest) == "gitlab"
+    assert get_service("git@github.ex.co:TankerHQ/tsrc.git", manifest) == "github_enterprise"
+    assert get_service("git@gitlab.ex.co:8443:TankerHQ/tsrc.git", manifest) == "gitlab"
+    assert get_service("git@github.ex.co:8443:TankerHQ/tsrc.git", manifest) == "github_enterprise"
+    # fmt: on
 
 
 def test_project_name_from_url() -> None:
@@ -65,11 +69,3 @@ def test_project_name_from_url() -> None:
 
     assert project_name("git@ex.co:foo/bar.git") == "foo/bar"
     assert project_name("ssh://git@ex.co:8022/foo/bar.git") == "foo/bar"
-
-
-def mock_workspace_git_urls() -> mock.Mock:
-    workspace_mock = mock.Mock(tsrc.Workspace)
-    workspace_mock.get_github_enterprise_url.return_value = "https://github.ex.co/"
-    workspace_mock.get_gitlab_url.return_value = "https://gitlab.ex.co/"
-
-    return workspace_mock
