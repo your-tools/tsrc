bf46a5c90100d151ac0c63ce8ab9acda61947b27
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-04-29 14:45:36 +0200

    Rewrite GitServer test helper using libgit2
    * Tests output should be much more readable
    * Should reduce full suite test time
    * No longer creates 2 git repos (one bare and one normal)
    
    This breaks a few things:
    
    * You cannot push a file containing a `/` because we only ever build
      one `Tree` object
    * You can specify a branch when calling `push_file`, but `change_branch`
      is gone

--- tsrc/test/helpers/git_server.py
+++ tsrc/test/helpers/git_server.py
@@ -2,38 +2,98 @@ from typing import cast, Any, Dict, List, Tuple
 import ruamel.yaml
 import pytest
 
-import tsrc
-import tsrc.git
-
 from path import Path
+import pygit2
 
 RepoConfig = Dict[str, Any]
 CopyConfig = Tuple[str, str]
 RemoteConfig = Tuple[str, str]
 
 
-class ManifestHandler:
+class TestRepo:
+    user = pygit2.Signature("Tasty Test", "test@tsrc.io")
+
     def __init__(self, path: Path) -> None:
-        self.path = path
+        self._repo = pygit2.Repository(str(path))
+
+    @classmethod
+    def create_bare(
+        cls, path: Path, initial_branch: str, empty: bool = False
+    ) -> "TestRepo":
+        repo = pygit2.init_repository(str(path), bare=True, initial_head=initial_branch)
+        if empty:
+            return cls(path)
+
+        blob_oid = repo.create_blob(b"this is the readme")
+        tree_builder = repo.TreeBuilder()
+        tree_builder.insert("README", blob_oid, pygit2.GIT_FILEMODE_BLOB)
+
+        repo.create_commit(
+            "HEAD", cls.user, cls.user, "initial commit", tree_builder.write(), []
+        )
+        return cls(path)
+
+    def create_tag(self, tag_name: str, *, branch: str, force: bool = False) -> None:
+        ref_name = "refs/heads/" + branch
+        ref = self._repo.references.get(ref_name)
+        assert ref
+        tag_ref = "refs/tags/" + tag_name
+        self._repo.create_reference(tag_ref, ref.target, force=force)
+
+    def ensure_ref(self, name: str) -> pygit2.Reference:
+        ref = self._repo.references.get(name)
+        if not ref:
+            ref = self._repo.create_reference(name, self._repo.head.target)
+        return ref
+
+    def commit_file(
+        self, name: str, *, branch: str, contents: str, message: str
+    ) -> pygit2.Tree:
+        assert "/" not in name, "creating subtrees is not supported"
+
+        ref_name = "refs/heads/" + branch
+        ref = self.ensure_ref(ref_name)
+        last_commit = self._repo.get(ref.target)
+        parents = [last_commit.oid]
+
+        old_tree = last_commit.tree
+        tree_builder = self._repo.TreeBuilder(old_tree)
+        blob_oid = self._repo.create_blob(contents.encode())
+        tree_builder.insert(name, blob_oid, pygit2.GIT_FILEMODE_BLOB)
+        new_tree = tree_builder.write()
+
+        author = self.user
+        commiter = self.user
+        self._repo.create_commit(ref_name, author, commiter, message, new_tree, parents)
+
+    def get_sha1(self) -> str:
+        return self._repo.head.target.hex  # type: ignore
+
+
+class ManifestHandler:
+    def __init__(self, repo: TestRepo) -> None:
+        self.repo = repo
         self.data = {"repos": []}  # type: Dict[str, Any]
+        self.branch = "master"
+        self.write_changes("Add an empty manifest")
 
-    @property
-    def yaml_path(self) -> Path:
-        return self.path / "manifest.yml"
+    def change_branch(self, new_branch: str) -> None:
+        self.repo.ensure_ref("refs/heads/" + new_branch)
+        self.branch = new_branch
 
-    def init(self) -> None:
+    def write_changes(self, message: str) -> None:
         to_write = ruamel.yaml.dump(self.data)
-        self.yaml_path.write_text(to_write)
-        tsrc.git.run(self.path, "add", "manifest.yml")
-        tsrc.git.run(self.path, "commit", "--message", "Add an empty manifest")
-        tsrc.git.run(self.path, "push", "origin", "master")
+        assert to_write
+        self.repo.commit_file(
+            "manifest.yml", contents=to_write, message=message, branch=self.branch
+        )
 
     def add_repo(self, src: str, url: str, branch: str = "master") -> None:
         repo_config = {"url": str(url), "src": src}
         if branch != "master":
             repo_config["branch"] = branch
         self.data["repos"].append(repo_config)
-        self.push(message="add %s" % src)
+        self.write_changes(message="add %s" % src)
 
     def configure_group(self, name: str, repos: List[str]) -> None:
         groups = self.data.get("groups")
@@ -42,7 +102,7 @@ class ManifestHandler:
             groups = self.data["groups"]
         groups[name] = {}
         groups[name]["repos"] = repos
-        self.push(message="add %s group" % name)
+        self.write_changes(message="add %s group" % name)
 
     def get_repo(self, src: str) -> RepoConfig:
         for repo in self.data["repos"]:
@@ -54,7 +114,7 @@ class ManifestHandler:
         repo = self.get_repo(src)
         repo[key] = value
         message = "Change %s %s: %s" % (src, key, value)
-        self.push(message)
+        self.write_changes(message)
 
     def set_repo_url(self, src: str, url: str) -> None:
         self.configure_repo(src, "url", url)
@@ -82,65 +142,39 @@ class ManifestHandler:
         repo["remotes"] = remote_dicts
         del repo["url"]
         message = "%s: remotes: %s" % (src, remote_dicts)
-        self.push(message)
-
-    def push(self, message: str) -> None:
-        to_write = ruamel.yaml.dump(self.data)
-        self.yaml_path.write_text(to_write)
-        tsrc.git.run(self.path, "add", "manifest.yml")
-        tsrc.git.run(self.path, "commit", "--message", message)
-        current_branch = tsrc.git.get_current_branch(self.path)
-        tsrc.git.run(self.path, "push", "origin", "--set-upstream", current_branch)
-
-    def change_branch(self, branch: str) -> None:
-        tsrc.git.run(self.path, "checkout", "-B", branch)
-        tsrc.git.run(
-            self.path, "push", "--no-verify", "origin", "--set-upstream", branch
-        )
+        self.write_changes(message)
 
 
 class GitServer:
     def __init__(self, tmpdir: Path) -> None:
         self.tmpdir = tmpdir
         self.bare_path = tmpdir / "srv"
-        self.src_path = tmpdir / "src"
-        self.add_repo("manifest", add_to_manifest=False)
-        self.manifest = ManifestHandler(self.get_path("manifest"))
-        self.manifest.init()
         self.manifest_url = self.get_url("manifest")
 
-    def get_path(self, name: str) -> Path:
-        return self.src_path / name
+        manifest_repo = self._create_repo("manifest")
+        self.manifest = ManifestHandler(manifest_repo)
 
     def get_url(self, name: str) -> str:
         return str("file://" + (self.bare_path / name))
 
+    def _get_repo(self, name: str) -> TestRepo:
+        repo_path = self.bare_path / name
+        return TestRepo(repo_path)
+
     def _create_repo(
         self, name: str, empty: bool = False, branch: str = "master"
-    ) -> str:
-        bare_path = self.bare_path / name
-        bare_path.makedirs_p()
-        tsrc.git.run(bare_path, "init", "--bare")
-        src_path = self.get_path(name)
-        src_path.makedirs_p()
-        tsrc.git.run(src_path, "init")
-        tsrc.git.run(src_path, "remote", "add", "origin", bare_path)
-        tsrc.git.run(bare_path, "symbolic-ref", "HEAD", "refs/heads/%s" % branch)
-        (src_path / "README").touch()
-        tsrc.git.run(src_path, "add", "README")
-        tsrc.git.run(src_path, "commit", "--message", "Initial commit")
-        if branch != "master":
-            tsrc.git.run(src_path, "checkout", "-b", branch)
-        if not empty:
-            tsrc.git.run(src_path, "push", "origin", "%s:%s" % (branch, branch))
-        return str(bare_path)
+    ) -> TestRepo:
+        repo_path = self.bare_path / name
+        repo_path.makedirs_p()
+        repo = TestRepo.create_bare(repo_path, initial_branch=branch, empty=empty)
+        return repo
 
     def add_repo(
         self,
         name: str,
-        add_to_manifest: bool = True,
         empty: bool = False,
         default_branch: str = "master",
+        add_to_manifest: bool = True,
     ) -> str:
         self._create_repo(name, empty=empty, branch=default_branch)
         url = self.get_url(name)
@@ -154,60 +188,28 @@ class GitServer:
         self.manifest.configure_group(group_name, repos)
 
     def push_file(
-        self, name: str, file_path: str, *, contents: str = "", message: str = ""
+        self,
+        name: str,
+        file_path: str,
+        *,
+        contents: str = "",
+        message: str = "",
+        branch: str = "master",
     ) -> None:
-        src_path = self.get_path(name)
-        full_path = src_path / file_path
-        full_path.parent.makedirs_p()
-        full_path.touch()
-        if contents:
-            full_path.write_text(contents)
-        commit_message = message or ("Create/Update %s" % file_path)
-        tsrc.git.run(src_path, "add", file_path)
-        tsrc.git.run(src_path, "commit", "--message", commit_message)
-        current_branch = tsrc.git.get_current_branch(src_path)
-        tsrc.git.run(src_path, "push", "origin", "--set-upstream", current_branch)
+        repo = self._get_repo(name)
+        if not message:
+            message = "add/update " + file_path
+        repo.commit_file(file_path, contents=contents, message=message, branch=branch)
 
-    def tag(self, name: str, tag_name: str, *, force: bool = False) -> None:
-        """ Create a new tag on the given repo. If `force` is True, tag is
-        deleted first so that it can be updated remotely.
-        """
-        src_path = self.get_path(name)
-        if force:
-            # Note:  Since we want to delete the previous tag if it exists,
-            # this command is allowed to fail:
-            tsrc.git.run(src_path, "tag", "-d", tag_name, check=False)
-        tsrc.git.run(src_path, "tag", tag_name)
-        cmd = [src_path, "push", "--no-verify", "origin", tag_name]
-        if force:
-            cmd.append("--force")
-        tsrc.git.run(*cmd)
-
-    def get_tags(self, name: str) -> List[str]:
-        src_path = self.get_path(name)
-        _, out = tsrc.git.run_captured(src_path, "tag")
-        return out.splitlines()
-
-    def get_branches(self, name: str) -> List[str]:
-        src_path = self.get_path(name)
-        _, out = tsrc.git.run_captured(src_path, "branch", "--list")
-        return [x[2:].strip() for x in out.splitlines()]
+    def tag(
+        self, name: str, tag_name: str, *, branch: str = "master", force: bool = False
+    ) -> None:
+        repo = self._get_repo(name)
+        repo.create_tag(tag_name, branch=branch, force=force)
 
     def get_sha1(self, name: str) -> str:
-        src_path = self.get_path(name)
-        _, out = tsrc.git.run_captured(src_path, "rev-parse", "HEAD")
-        return out
-
-    def change_repo_branch(self, name: str, new_branch: str) -> None:
-        src_path = self.get_path(name)
-        tsrc.git.run(src_path, "checkout", "-B", new_branch)
-        tsrc.git.run(
-            src_path, "push", "--no-verify", "origin", "--set-upstream", new_branch
-        )
-
-    def delete_branch(self, name: str, branch: str) -> None:
-        src_path = self.get_path(name)
-        tsrc.git.run(src_path, "push", "origin", "--delete", branch)
+        repo = self._get_repo(name)
+        return repo.get_sha1()
 
 
 @pytest.fixture
--- tsrc/test/test_test_helpers.py
+++ tsrc/test/test_test_helpers.py
@@ -16,6 +16,10 @@ def read_remote_manifest(workspace_path: Path, git_server: GitServer) -> tsrc.Ma
 def test_git_server_add_repo_can_clone(
     workspace_path: Path, git_server: GitServer
 ) -> None:
+    """ Check that repo added to the GitServer can be cloned,
+    typically, they should be bare but not empty!
+
+    """
     foobar_url = git_server.add_repo("foo/bar")
     tsrc.git.run(workspace_path, "clone", foobar_url)
     assert (workspace_path / "bar").exists()
@@ -42,26 +46,28 @@ def test_git_server_add_repo_updates_manifest(
         assert "refs/heads/master" in out
 
 
-def test_git_server_change_manifest_branch(
+def test_git_server_multiple_manifest_branches(
     workspace_path: Path, git_server: GitServer
 ) -> None:
     git_server.add_repo("foo")
     git_server.manifest.change_branch("devel")
     git_server.add_repo("bar")
 
-    tsrc.git.run(workspace_path, "clone", git_server.manifest_url, "--branch", "devel")
+    tsrc.git.run(workspace_path, "clone", git_server.manifest_url)
     manifest_yml = workspace_path / "manifest/manifest.yml"
     manifest = tsrc.manifest.load(manifest_yml)
+    assert len(manifest.get_repos()) == 1
 
+    tsrc.git.run(workspace_path / "manifest", "reset", "--hard", "origin/devel")
+    manifest = tsrc.manifest.load(manifest_yml)
     assert len(manifest.get_repos()) == 2
 
 
-def test_git_server_change_repo_branch(
+def test_git_server_push_to_other_branch(
     workspace_path: Path, git_server: GitServer
 ) -> None:
     foo_url = git_server.add_repo("foo")
-    git_server.change_repo_branch("foo", "devel")
-    git_server.push_file("foo", "devel.txt", contents="this is devel\n")
+    git_server.push_file("foo", "devel.txt", contents="this is devel\n", branch="devel")
     tsrc.git.run(workspace_path, "clone", foo_url, "--branch", "devel")
     foo_path = workspace_path / "foo"
     assert (foo_path / "devel.txt").text() == "this is devel\n"
@@ -69,9 +75,15 @@ def test_git_server_change_repo_branch(
 
 def test_git_server_tag(workspace_path: Path, git_server: GitServer) -> None:
     foo_url = git_server.add_repo("foo")
-    git_server.tag("foo", "v0.1")
+    git_server.tag("foo", "v0.1", branch="master")
     _, out = tsrc.git.run_captured(workspace_path, "ls-remote", foo_url)
     assert "refs/tags/v0.1" in out
+
+
+def test_git_server_get_sha1(workspace_path: Path, git_server: GitServer) -> None:
+    git_server.add_repo("foo")
+    actual = git_server.get_sha1("foo")
+    assert type(actual) == str
 
 
 def test_git_server_default_branch_devel(
