84d31fa7801312aabb992193ac95801ecae6b34c
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2020-06-16 17:42:03 +0200

    lint.sh: also run black

--- lint.sh
+++ lint.sh
@@ -1,6 +1,7 @@
 set -x
 set -e
 
+poetry run black --check tsrc
 poetry run flake8 .
 poetry run mypy
 poetry run mkdocs build
--- tsrc/test/test_workspace_config.py
+++ tsrc/test/test_workspace_config.py
@@ -15,7 +15,7 @@ def test_save(tmp_path: Path) -> None:
         shallow_clones=True,
         repo_groups=["default", "a-team"],
         clone_all_repos=False,
-        singular_remote=None
+        singular_remote=None,
     )
     persistent_path = tmp_path / "config.yml"
     config.save_to_file(persistent_path)
