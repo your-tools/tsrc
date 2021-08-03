b10a6ee3acd218e40dc8df457846533ee33aea94
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-08-03 15:15:29 +0200

    Cleanup test_executor
    * Add some useful doc strings.
    * Use proper __init__() for the Kaboom exception
    * Use a less magical value for causing the FakeTask to fail
    * Add missing on_failure() override in FakeTask

--- tsrc/test/test_executor.py
+++ tsrc/test/test_executor.py
@@ -6,23 +6,33 @@ from tsrc.executor import ExecutorFailed, Task, run_sequence
 
 
 class Kaboom(Error):
-    def __str__(self) -> str:
-        return "Kaboom!"
+    def __init__(self) -> None:
+        self.message = "Kaboom"
 
 
 class FakeTask(Task[str]):
+    """This is a fake Task that can be used for testing.
+
+    Note that it will raise an instance of the Kaboom exception
+    when processing an item whose value is "failing"
+
+    """
+
     def __init__(self) -> None:
         pass
 
     def on_start(self, *, num_items: int) -> None:
         ui.info("Frobnicating", num_items, "items")
 
+    def on_failure(self, *, num_errors: int) -> None:
+        ui.error("Failed to frobnicate some items")
+
     def display_item(self, item: str) -> str:
         return item
 
     def process(self, index: int, count: int, item: str) -> None:
         ui.info_count(index, count, "frobnicate", item)
-        if item == "bar":
+        if item == "failing":
             print("ko :/")
             raise Kaboom()
         ui.info("ok !")
@@ -35,10 +45,10 @@ def test_doing_nothing() -> None:
 
 def test_happy() -> None:
     task = FakeTask()
-    run_sequence(["foo", "spam"], task)
+    run_sequence(["foo", "bar"], task)
 
 
 def test_collect_errors() -> None:
     task = FakeTask()
     with pytest.raises(ExecutorFailed):
-        run_sequence(["foo", "bar"], task)
+        run_sequence(["foo", "failing", "bar"], task)
