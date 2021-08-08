fe189c38ddf8982e812bcb9185ddc776164a0571
Dimitri Merejkowsky <dimitri@dmerej.info>
2021-08-08 18:12:12 +0200

    Remove `tsrc version` command
    Running tsrc --version works just as well

--- tsrc/test/cli/test_main.py
+++ tsrc/test/cli/test_main.py
@@ -13,3 +13,9 @@ def test_help() -> None:
     with pytest.raises(SystemExit) as e:
         main(["-h"])
     assert e.value.code == 0
+
+
+def test_version() -> None:
+    with pytest.raises(SystemExit) as e:
+        main(["--version"])
+    assert e.value.code == 0
--- tsrc/test/cli/test_version.py
+++
@@ -1,8 +0,0 @@
-from cli_ui.tests import MessageRecorder
-
-from tsrc.test.helpers.cli import CLI
-
-
-def test_version(tsrc_cli: CLI, message_recorder: MessageRecorder) -> None:
-    tsrc_cli.run("version")
-    assert message_recorder.find("version")
